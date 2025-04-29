464773e564e186a6f1ce7ad4f36571eae9062d6e
TolMen Jy <cotton.frachisse@gmail.com>
2025-04-29 15:48:37 +0200

    Progress on the links

--- src/component/navbar.php
+++ src/component/navbar.php
@@ -1,11 +1,7 @@
-<?php
-$current_page = basename($_SERVER['PHP_SELF']); // Récupère uniquement le nom du fichier actuel
-?>
-
 <nav class="navbar navbar-expand-xl navbar-light bg-light fixed-top custom-navbar">
     <div class="container-fluid">
-        <a href="#top">
-            <span class="navbar-brand">Civilipédia</span>
+        <a href="#liens">
+            <span class="navbar-brand"><strong>Jessy Frachisse</strong></span>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -13,45 +9,20 @@ $current_page = basename($_SERVER['PHP_SELF']); // Récupère uniquement le nom 
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link custom-nav-link <?php echo ($current_page == 'home.php') ? 'active text-primary' : ''; ?>" href="home.php">Accueil</a>
+                    <a class="nav-link custom-nav-link" href="#liens">Liens</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link custom-nav-link <?php echo ($current_page == 'contact.php') ? 'active text-primary' : ''; ?>" href="contact.php">Contact</a>
+                    <a class="nav-link custom-nav-link" href="#skills">Skills</a>
                 </li>
 
-                <?php if (isset($_SESSION['userRole']) && $_SESSION['userRole'] == 'admin'): ?>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link custom-nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Administration</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="article_list.php">Gestion des articles</a></li>
-                            <li><a class="dropdown-item" href="user_list.php">Gestion des utilisateurs</a></li>
-                            <li><a class="dropdown-item" href="messagerie.php">Messagerie</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item text-danger" href="src/control/ArtControl/resetAllArt.php">Réinitialiser les articles</a></li>
-                            <li><a class="dropdown-item text-danger" href="src/control/UserControl/resetAllUser.php">Réinitialiser les utilisateurs</a></li>
-                        </ul>
-                    </li>
+                <li class="nav-item">
+                    <a class="nav-link custom-nav-link" href="#projets">Projets</a>
+                </li>
 
-                <?php endif; ?>
-
-                <?php if (!empty($_SESSION['userID'])): ?>
-                    <li class="nav-item">
-                        <a class="nav-link custom-nav-link <?php echo ($current_page == 'createArt.php') ? 'active text-primary' : ''; ?>" href="createArt.php">Publier un article</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-danger" href="src/control/UserControl/logout.php" title="Déconnexion">Déconnexion</a>
-                    </li>
-                <?php else: ?>
-                    <li class="nav-item">
-                        <a class="nav-link custom-nav-link <?php echo ($current_page == 'login.php') ? 'active text-primary' : ''; ?>" href="login.php">Connexion</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link custom-nav-link <?php echo ($current_page == 'register.php') ? 'active text-primary' : ''; ?>" href="register.php">Inscription</a>
-                    </li>
-                <?php endif; ?>
+                <li class="nav-item">
+                    <a class="nav-link custom-nav-link" href="#contact">Contactez-moi</a>
+                </li>
             </ul>
         </div>
     </div>
