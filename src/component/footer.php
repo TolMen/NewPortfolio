3930c55c5c296c2b8936866aced0b847e2d11fcd
TolMen Jy <cotton.frachisse@gmail.com>
2025-05-05 20:53:11 +0200

    Added a call me button

--- src/component/footer.php
+++ src/component/footer.php
@@ -10,7 +10,7 @@
             date_default_timezone_set("Europe/Paris");
             echo date("Y");
             ?>
-            Jessy | Tous les droits sont réservés. <br />
+            Jessy Frachisse | Tous les droits sont réservés. <br />
         </p>
 
         <!-- Social media and CV links -->
@@ -24,6 +24,7 @@
             <a href="public/assets/files/CVJessy.png" target="_blank" title="CV">
                 <i class="fa-solid fa-file-lines"></i>
             </a>
+            <a href="tel:+33667689169" title="Téléphonez-moi"><i class="fa-solid fa-phone-volume"></i></a>
         </nav>
 
     </div>
